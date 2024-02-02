2f7095b4b969430fa6607932442374d027c08c1d
gabriel.dumont <gabriel.dumont@e-core.com>
2024-02-02 09:53:44 -0300

    Adjusted base test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/glass2do/i)
   expect(linkElement).toBeInTheDocument()
 })
