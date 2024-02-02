f586c0bbbe1325fc6e767d2854e8c42e77f2eb16
gabriel.dumont <gabriel.dumont@e-core.com>
2024-02-02 12:41:56 -0300

    Added jest into project

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^contexts/(.*)$': '<rootDir>/src/contexts/$1',
+    '^types/(.*)$': '<rootDir>/src/types/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
